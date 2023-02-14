c5cb48e02b5d4a22bb9c4ab9391f54d28954fe4d
Rex <s46129@yahoo.com.tw>
2023-02-14 14:42:01 +0800

    feat : replaceAll

--- Runtime/Script/TagReplaceDictionary.cs
+++ Runtime/Script/TagReplaceDictionary.cs
@@ -13,17 +13,16 @@ namespace StringTagReplace.Runtime.Script
         [FormerlySerializedAs("_dictionary")] [SerializeField]
         private List<Tag> dictionary = new();
 
-        public string GetValue(string key)
-        {
-            foreach (var tag in dictionary.Where(tag => tag.key == key)) return tag.value;
-            return key;
-        }
-
         public void InsertData(Tag tag)
         {
             dictionary ??= new List<Tag>();
             var tags = dictionary.Where(find => find.key == tag.key).ToList();
             if (!tags.Any()) dictionary.Add(tag);
+        }
+
+        public string ReplaceAll(string data)
+        {
+            return dictionary.Aggregate(data, (current, tag) => current.Replace(tag.key, tag.value));
         }
     }
 
