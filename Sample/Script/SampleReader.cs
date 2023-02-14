2f805d612a9727df61cd578344f99943a11dd931
Rex <s46129@yahoo.com.tw>
2023-02-14 14:42:22 +0800

    test : Add Test ReplaceAll

--- Sample/Script/SampleReader.cs
+++ Sample/Script/SampleReader.cs
@@ -2,18 +2,19 @@
 using StringTagReplace.Runtime.Script;
 using UnityEngine;
 
-namespace Sample.Script
+namespace StringTagReplace.Sample.Script
 {
     public class SampleReader : MonoBehaviour
     {
         [SerializeField] private TextAsset data;
+        [SerializeField] private TagReplaceDictionary _dictionary;
 
         [Button]
+        [HideIf("@data==null||_dictionary==null")]
         private void Test()
         {
-            string dataText = data.text;
-
-            TagFinder.MatchTag(dataText);
+            var replaceValue = _dictionary.ReplaceAll(data.text);
+            Debug.Log($"{data.text} =>{replaceValue}");
         }
     }
 }
--- Test/TestReplaceTag.cs
+++ Test/TestReplaceTag.cs
@@ -8,11 +8,15 @@ namespace StringTagReplace.Test
     {
         [TestCase("<button=1>", "<sprite=3>")]
         [TestCase("<button=1>", "<button=3>")]
-        public void TestGetValue(string key, string value)
+        [TestCase("<button=1>", "<button=1>")]
+        public void TestReplaceAll(string key, string value)
         {
             var tagReplaceDictionary = ScriptableObject.CreateInstance<TagReplaceDictionary>();
             tagReplaceDictionary.InsertData(new Tag { key = key, value = value });
-            Assert.AreEqual(tagReplaceDictionary.GetValue(key), value);
+            var testData = $"This Tag {key} be replace";
+            var actual = tagReplaceDictionary.ReplaceAll(testData);
+            var assertion = $"This Tag {value} be replace";
+            Assert.AreEqual(assertion, actual);
         }
     }
 }
